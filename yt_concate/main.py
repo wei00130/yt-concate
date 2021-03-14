91297c23c27bc86612174bb2de4bd8146abe4444
wei00130 <wei00130@gmail.com>
2021-03-14 18:13:05 +0800

    Successfully got video urls from from channel and add requirements.txt

---
+++ yt_concate/main.py
@@ -0,0 +1,39 @@
+import urllib.request as urlrq
+import json
+import ssl
+import certifi
+from yt_concate.settings import API_KEY
+
+print(API_KEY)
+
+CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
+
+
+def get_all_video_in_channel(channel_id):
+
+    base_video_url = 'https://www.youtube.com/watch?v='
+    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+
+    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key,
+                                                                                                        channel_id)
+
+    video_links = []
+    url = first_url
+    while True:
+        inp = urlrq.urlopen(url, context=ssl.create_default_context(cafile=certifi.where()))
+        resp = json.load(inp)
+
+        for i in resp['items']:
+            if i['id']['kind'] == "youtube#video":
+                video_links.append(base_video_url + i['id']['videoId'])
+
+        try:
+            next_page_token = resp['nextPageToken']
+            url = first_url + '&pageToken={}'.format(next_page_token)
+        except KeyError:
+            break
+    return video_links
+
+
+# video_list = get_all_video_in_channel(CHANNEL_ID)
+# print(len(video_list))
